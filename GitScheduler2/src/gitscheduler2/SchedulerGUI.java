4acb1db2b7a1f6660d2bbe0581fa0c0ebfee3061
k00108666 <seamo123@gmail.com>
2015-01-07 12:11:48 -0700

    Made SchedulerGUI Classe

---
+++ GitScheduler2/src/gitscheduler2/SchedulerGUI.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitscheduler2;
+
+/**
+ *
+ * @author Seamus O Connor
+ */
+public class SchedulerGUI {
+    
+}
