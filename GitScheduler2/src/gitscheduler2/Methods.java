1df1ed264a96a7b7e7979d9f49c126479b95153b
k00108666 <seamo123@gmail.com>
2015-01-14 16:06:37 -0700

    Creating tables perfectly. Changed date to a global variable.

--- GitScheduler2/src/gitscheduler2/Methods.java
+++ GitScheduler2/src/gitscheduler2/Methods.java
@@ -32,7 +32,7 @@ public  class Methods {
     ResultSet rs;
     ResultSet rs2;
     String sql;
-    
+  
     
     
     
@@ -109,20 +109,19 @@ public final void doConnect() {
                 
                 
               try {
-                 this.doConnect();;
-                  SchedulerGUI gui = new SchedulerGUI();
-                stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-                
-                  Calendar cal = Calendar.getInstance();
-                cal.setTime(gui.setSelectedDate());
+                 this.doConnect();
+               
+                 stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+                //SchedulerGUI gui = new SchedulerGUI();
+                 Calendar cal = Calendar.getInstance();
+                    cal.setTime(SchedulerGUI.date);
                  int WeekNum = cal.get(Calendar.WEEK_OF_YEAR);
-              System.out.println(Integer.toString(WeekNum));
-            int year = cal.get(Calendar.YEAR);
-                       System.out.println(Integer.toString(year));
-           String DatabaseString = "CREATE TABLE " + Integer.toString(year) + "_week_" + Integer.toString(WeekNum) + " (PeriodNum INT DEFAULT 1  NOT NULL, Monday VARCHAR(30), Tuesday VARCHAR(30), Wednesday VARCHAR(30), Thursday VARCHAR(30), Friday VARCHAR(30), Saturday VARCHAR(30), Sunday VARCHAR(30), PRIMARY KEY (PeriodNum))";
+                 int year = cal.get(Calendar.YEAR);
+                      
+                 String DatabaseString = "CREATE TABLE " + Integer.toString(year) + "_week_" + Integer.toString(WeekNum) + " (PeriodNum INT DEFAULT 1  NOT NULL, Monday VARCHAR(30), Tuesday VARCHAR(30), Wednesday VARCHAR(30), Thursday VARCHAR(30), Friday VARCHAR(30), Saturday VARCHAR(30), Sunday VARCHAR(30), PRIMARY KEY (PeriodNum))";
                  System.out.println(DatabaseString);
                 
-                stmt.executeUpdate(DatabaseString);
+             stmt.executeUpdate(DatabaseString);
              } catch (SQLException ex) {
                  Logger.getLogger(Methods.class.getName()).log(Level.SEVERE, null, ex);
                   JOptionPane.showMessageDialog(null, "This schedule already exists. Please update it or select another week.");
